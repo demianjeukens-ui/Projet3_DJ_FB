7c46140cdedabc834c830237f9851c227c6a55e2
Demian Jeukens <demian.jeukens@gmail.com>
2025-12-13 16:43:41 +0100

    hclustGetClustersDist V1.0

--- HierarchicalClustering.c
+++ HierarchicalClustering.c
@@ -60,3 +60,64 @@ int hclustNBLeaves(Hclust *hc)
     BTNode *root = btRoot(hc->tree);    // on récupère la racine
     return nbLeavesRec(hc->tree, root); // on appelle la fct recursive avec la racine
 }
+
+/// Fonctionns statiques d'aide pour hclustClustersDist ///
+
+static void collectLeavesRec(BTree *tree, BTNode *n, List *out) // fct recursive pour faire une liste des feuilles d'un sous-arbre/cluster(methode plus facile qu'avec mapleaves)
+{
+    if (n == NULL)
+        return;
+
+    if (btIsExternal(tree, n)) // si on est sur une feuille
+    {
+        llInsertLast(out, btGetData(tree, n)); // on ajoute la data de la feuille à la liste
+        return;
+    }
+
+    collectLeavesRec(tree, btLeft(tree, n), n);  // on descend dans le sous-arbre gauche
+    collectLeavesRec(tree, btRight(tree, n), n); // on descend dans le sous-arbre droit
+}
+
+static void clustersDistRec(BTree *tree, BTNode *n, BTNode *parent, double T, List *clusters) // T pour threshold(plus court)
+{
+    if (n == NULL)
+        return;
+
+    if (btIsInternal(tree, n)) // verifie que c'est un noeud interne
+    {
+        double dn = *(double *)btGetData(tree, n); // distance du noeud courant(on cast le retour de GetData en double)
+
+        int parentAbove = 1; // boolean pour savoir si le parent est au dessus du seuil T(1 = true, 0 = false)
+        if (parent != NULL)  // sinon tout l'arbre est en dessous du seuil
+        {
+            double dp = *(double *)btGetData(tree, parent); // distance du parent
+            parentAbove = (dp > T);                         // on verifie si le parent est au dessus du seuil
+        }
+
+        // creation d'un cluster si conditions remplies///
+
+        if (dn <= T && parentAbove) // verifie que distance avec noeud en dessous < T < distance avec parent au dessus(conditions pour former un cluster)
+        {
+            List *new = llCreateEmpty();    // on cree une "new"list pour stocker les feuilles du cluster
+            collectLeavesRec(tree, n, new); // on collecte les feuilles du cluster
+            llInsertLast(clusters, new);    // on ajoute le "new"cluster a la liste des clusters
+            return;
+        }
+    }
+
+    // sinon on cherche plus bas
+
+    clustersDistRec(tree, btLeft(tree, n), n, T, clusters);
+    clustersDistRec(tree, btRight(tree, n), n, T, clusters);
+}
+
+List *hclustGetClustersDist(Hclust *hc, double distanceThreshold)
+{
+    List *clusters = llCreateEmpty();   // liste de clusters a retourner
+    if (hc == NULL || hc->tree == NULL) // si jamais struct hc ou arbre dedans NULL
+        return clusters;
+
+    BTNode *root = btRoot(hc->tree);                                    // on récupère la racine
+    clustersDistRec(hc->tree, root, NULL, distanceThreshold, clusters); // on appelle la fct recursive avec la racine
+    return clusters;
+}
