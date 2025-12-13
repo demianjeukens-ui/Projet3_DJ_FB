39f700b11b558900de85a78a893505426761773d
Florent Bonnechère <florent.bon222@gmail.com>
2025-12-13 18:20:22 +0100

    Debut hclustBuildTree + Fonctions annexes

--- HierarchicalClustering.c
+++ HierarchicalClustering.c
@@ -1,10 +1,71 @@
 #include <stdlib.h>
+#include <string.h> // Pour strdup
+#include <stdio.h>
 #include "HierarchicalClustering.h"
+#include "BTree.h"
+#include "List.h"
+#include "Dict.h"
 
 struct Hclust_t
 {
-    BTree *tree;
+    BTree *finaltree;
 };
+
+// Strucutre servant au stockage
+typedef struct
+{
+    const char *o1; // Nom objet 1
+    const char *o2; // Nom objet 2
+    double dist;    // Distance entre les 2 objets
+} Pair_t;
+
+// Structure permettant de passer les nouveaux paramètres(Dict et nouveau cluster) en arguments de btMapLeaves
+typedef struct
+{
+    Dict *dict;
+    BTree *new_cluster;
+} New_params;
+
+// Compare les distances des pairs A et B
+static int compare_pairs(const void *a, const void *b)
+{
+
+    const Pair_t **Pair_A = (const Pair_t **)a;
+    const Pair_t **Pair_B = (const Pair_t **)b;
+
+    double dist_A = (*Pair_A)->dist;
+    double dist_B = (*Pair_B)->dist;
+
+    if (dist_A < dist_B)
+        return -1;
+
+    if (dist_A > dist_B)
+        return 1;
+
+    return 0;
+}
+
+// Fonction appelée par btMapLeaves pour mettre à jour le dictionnaire
+static void update_dict(void *data, void *fparams)
+{
+    const char *object_name = (const char *)data;
+    New_params *params = (New_params *)fparams;
+
+    // Ecrase l'ancien nom de cluster par le nouveau
+    dictInsert(params->dict, object_name, params->new_cluster);
+}
+
+Hclust *hclustBuildTree(List *objects, double (*distFn)(const char *, const char *, void *), void *distFnParams)
+{
+    int N = listSize(objects);
+    if (N == 0)
+        return NULL;
+
+    Hclust *hc = malloc(sizeof(Hclust));
+    if (!hc)
+        exit(EXIT_FAILURE);
+    hc->finaltree = NULL;
+}
 
 /// Partie ajoutée DEM ///
 
